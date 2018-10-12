24924abe00d783e2b985999106a5bf0ce16c3ede
LiDong <ldwqh0@outlook.com>
2018-10-12 16:06:01 +0800

    增加注释

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-export chooseImage from './chooseImage'
-export closeWindow from './closeWindow'
-export getLocation from './getLocation'
-export { scanQRCode } from './qrCode'
-export selectPeoples from './selectPeoples'
-export previewImage from './previewImage'
+export chooseImage from './chooseImage' //选择图像，包括拍照
+export closeWindow from './closeWindow' // 关闭窗口
+export getLocation from './getLocation' // 定位
+export { scanQRCode } from './qrCode' // 二维码扫描
+export selectPeoples from './selectPeoples' // 人员选择
+export previewImage from './previewImage' // 图片预览
