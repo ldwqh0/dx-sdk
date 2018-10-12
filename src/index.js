5fc2e9b91c1c192ab1ef171382662043a135bee6
LiDong <ldwqh0@outlook.com>
2018-10-12 16:16:52 +0800

    调整项目结构

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-export chooseImage from './chooseImage' //选择图像，包括拍照
-export closeWindow from './closeWindow' // 关闭窗口
-export getLocation from './getLocation' // 定位
-export { scanQRCode } from './qrCode' // 二维码扫描
-export selectPeoples from './selectPeoples' // 人员选择
-export previewImage from './previewImage' // 图片预览
+export chooseImage from './lib/chooseImage' //选择图像，包括拍照
+export closeWindow from './lib/closeWindow' // 关闭窗口
+export getLocation from './lib/getLocation' // 定位
+export { scanQRCode } from './lib/qrCode' // 二维码扫描
+export selectPeoples from './lib/selectPeoples' // 人员选择
+export previewImage from './lib/previewImage' // 图片预览
